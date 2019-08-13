ee0bcfdcccafc1d8c5b34e063c3bf0b817fc50b4
aileesh4 <53839808+aileesh4@users.noreply.github.com>
2019-08-12 23:34:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+  // let score = score + 1
+  
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  // let score = score + 1
+}
+
+let score = 0
+
+const button = document.querySelector('#done')
+const greeting = document.querySelector('#greeting')
+const input = document.querySelector('input')
+
+
+button.addEventListener('click', (e) => {
+  greeting.innerHTML = "nice job"
+  
+})
+
